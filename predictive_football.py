123f84cae4c18a53a6780b48ec91c179627d8fa1
jrglamber <jrglamber@hotmail.com>
2026-09-25 07:36:10 +0100

    Restore football_predictive engine prediction query

--- predictive_football.py
+++ predictive_football.py
@@ -870,7 +870,7 @@ class PredictiveFootballEngine:
             )
         )
         predictions = self.db.fetchall(
-            "SELECT home_probability,draw_probability,away_probability,home_team,away_team,actual_outcome,brier_score,log_loss,model_brier_advantage,closing_market_quality FROM football_predictive_predictions"
+            "SELECT * FROM football_predictive_predictions ORDER BY id"
         )
         created = 0
         for pred in predictions:
